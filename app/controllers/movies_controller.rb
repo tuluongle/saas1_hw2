05be4d11d15f61853b3cc4542cf2848975a25cf8
saasbook <saasbook@saasbook.(none)>
2013-08-01 04:25:05 -0700

    add session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,21 +12,33 @@ class MoviesController < ApplicationController
   end
 
   def index
+    redirect= false
     if params[:sort]
-	 @sorting = params[:sort]
+      @sorting = params[:sort]
+    elsif session[:sort]
+      @sorting = session[:sort]
+      redirect= true
     end
     if params[:ratings]
-	 @ratings = params[:ratings]
+      @ratings = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect= true
     else
       @all_ratings.each do |rat|
         (@ratings ||= { })[rat] = 1
       end
+    end
+    if redirect
+      redirect_to movies_path(:sort => @sorting, :ratings => @ratings)
     end
     Movie.find(:all, :order => @sorting ? @sorting : :id).each do |mv|
       if (@ratings.keys.include? mv[:rating])
         (@movies ||= [ ]) << mv
       end
     end
+    session[:sort]= @sorting
+    session[:ratings]= @ratings
   end
 
   def new
